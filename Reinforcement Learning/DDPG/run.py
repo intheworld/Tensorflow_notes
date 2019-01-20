071e7986073ba9a82ae7cf9a873a035ee1868224
swliu <swliu@cam2cloud.com>
2019-01-20 21:47:53 +0800

    add DDPG implementation

---
+++ Reinforcement Learning/DDPG/run.py
@@ -0,0 +1,67 @@
+import gym
+import matplotlib.pyplot as plt
+from ddpg import DDPG
+
+GAME = 'Pendulum-v0'
+MAX_STEP = 100
+EPISODES = 3000
+
+env = gym.make(GAME)
+env = env.unwrapped
+env.seed(1)
+
+n_states = env.observation_space.shape[0]
+n_actions = env.action_space.shape[0]
+action_low_bound = env.action_space.low
+action_high_bound = env.action_space.high
+
+print(action_low_bound)
+print(action_high_bound)
+
+agent = DDPG(n_states, n_actions, action_low_bound, action_high_bound)
+RENDER = True
+
+
+def run():
+    plt.ion()
+    total_r = 0
+    avg_ep_r_hist = []
+    for episode in range(EPISODES):
+        ep_step = 0
+        ep_r = 0
+        s = env.reset()
+        while True:
+            if RENDER:
+                env.render()
+            a = agent.choose_action(s)
+            s_, r, done, info = env.step(a)
+            agent.store_memory(s, a, r, s_)
+
+            ep_r += r
+            total_r += r
+            ep_step += 1
+
+            if agent.memory_counter >= agent.batch_size:
+                agent.learn()
+
+            if ep_step >= MAX_STEP:
+                break
+
+            s = s_
+
+        if episode >= 10:
+            avg_ep_r = total_r/(episode+1)
+            avg_ep_r_hist.append(avg_ep_r)
+            if episode % 20 == 0:
+                print('Episode %d Avg Reward/Ep %s' % (episode, avg_ep_r))
+
+        plt.cla()
+        plt.plot(avg_ep_r_hist)
+        plt.pause(0.0001)
+    plt.ioff()
+    plt.show()
+
+
+if __name__ == '__main__':
+    run()
+
