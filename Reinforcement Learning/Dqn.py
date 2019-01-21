a368dfb7dc322e0ec94f624c9ac26083696ecf85
swliu <liushuwei@xiaomi.com>
2019-01-21 13:03:38 +0800

    add policy gradient

--- Reinforcement Learning/Dqn.py
+++ Reinforcement Learning/Dqn.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
 
 np.random.seed(1)
 tf.set_random_seed(1)
@@ -132,7 +133,6 @@ class DeepQNetwork:
         self.learn_step_counter += 1
 
     def plot_cost(self):
-        import matplotlib.pyplot as plt
         plt.plot(np.arange(len(self.cost_his)), self.cost_his)
         plt.ylabel('Cost')
         plt.xlabel('training steps')
