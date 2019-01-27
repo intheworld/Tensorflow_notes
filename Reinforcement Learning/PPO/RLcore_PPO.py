c3239112c1d9fcea7377b40db93d1be581158b39
swliu <swliu@cam2cloud.com>
2019-01-27 10:47:10 +0800

    add filter for PPO

--- Reinforcement Learning/PPO/RLcore_PPO.py
+++ Reinforcement Learning/PPO/RLcore_PPO.py
@@ -1,7 +1,5 @@
 import tensorflow as tf
 import numpy as np
-import matplotlib.pyplot as plt
-
 
 METHOD = [
     dict(name='kl_pen', kl_target=0.01, lam=0.5),   # KL penalty
@@ -16,38 +14,28 @@ S_DIM, A_DIM = 3, 1
 
 
 class PPO(object):
-
-    def __init__(self, gamma=0.9, tau=0.1):
+    def __init__(self):
         self.sess = tf.Session()
-        self.gamma = gamma
-        self.tau = tau
         self.tfs = tf.placeholder(tf.float32, [None, S_DIM], 'state')
-        self.tfs_ = tf.placeholder(tf.float32, [None, S_DIM], 'state_')
-        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
-        self.ret_ph = tf.placeholder(tf.float32, [None, 1], name='v')
+
+        # critic
+        with tf.variable_scope('critic'):
+            l1 = tf.layers.dense(self.tfs, 100, tf.nn.relu)
+            self.v = tf.layers.dense(l1, 1)
+            self.tfdc_r = tf.placeholder(tf.float32, [None, 1], 'discounted_r')
+            self.advantage = self.tfdc_r - self.v
+            self.closs = tf.reduce_mean(tf.square(self.advantage))
+            self.ctrain_op = tf.train.AdamOptimizer(C_LR).minimize(self.closs)
 
         # actor
         pi, pi_params = self._build_anet('pi', trainable=True)
         oldpi, oldpi_params = self._build_anet('oldpi', trainable=False)
-
-        act = tf.clip_by_value(tf.squeeze(pi.sample(1), axis=0), -2, 2)
-
-        # critic
-        self.critic_net = self._build_critic_net(scope='critic')
-
-        self.ce_params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='critic')
-
-        with tf.variable_scope('critic_loss'):
-            self.critic_loss_op = tf.reduce_mean(tf.squared_difference(self.ret_ph, self.critic_net))
-
-        with tf.variable_scope('critic_train'):
-            self.critic_train_op = tf.train.AdamOptimizer(C_LR).minimize(self.critic_loss_op, var_list=self.ce_params)
-
         with tf.variable_scope('sample_action'):
             self.sample_op = tf.squeeze(pi.sample(1), axis=0)       # choosing action
         with tf.variable_scope('update_oldpi'):
             self.update_oldpi_op = [oldp.assign(p) for p, oldp in zip(pi_params, oldpi_params)]
 
+        self.tfa = tf.placeholder(tf.float32, [None, A_DIM], 'action')
         self.tfadv = tf.placeholder(tf.float32, [None, 1], 'advantage')
         with tf.variable_scope('loss'):
             with tf.variable_scope('surrogate'):
@@ -71,11 +59,10 @@ class PPO(object):
 
         self.sess.run(tf.global_variables_initializer())
 
-    def update(self, s, a, r, bv):
+    def update(self, s, a, r):
         self.sess.run(self.update_oldpi_op)
-        v = self.sess.run(self.critic_net, {self.tfs: s})
-        adv = bv - v
-        adv = (adv - adv.mean())/(adv.std()+1e-6)     # sometimes helpful
+        adv = self.sess.run(self.advantage, {self.tfs: s, self.tfdc_r: r})
+        # adv = (adv - adv.mean())/(adv.std()+1e-6)     # sometimes helpful
 
         # update actor
         if METHOD['name'] == 'kl_pen':
@@ -89,12 +76,12 @@ class PPO(object):
                 METHOD['lam'] /= 2
             elif kl > METHOD['kl_target'] * 1.5:
                 METHOD['lam'] *= 2
-            METHOD['lam'] = np.clip(METHOD['lam'], 1e-4, 10)   # sometimes explode, this clipping is my solution
+            METHOD['lam'] = np.clip(METHOD['lam'], 1e-4, 10)    # sometimes explode, this clipping is my solution
         else:   # clipping method, find this is better (OpenAI's paper)
             [self.sess.run(self.atrain_op, {self.tfs: s, self.tfa: a, self.tfadv: adv}) for _ in range(A_UPDATE_STEPS)]
 
         # update critic
-        [self.sess.run(self.critic_train_op, {self.tfs: s, self.ret_ph: bv}) for _ in range(C_UPDATE_STEPS)]
+        [self.sess.run(self.ctrain_op, {self.tfs: s, self.tfdc_r: r}) for _ in range(C_UPDATE_STEPS)]
 
     def _build_anet(self, name, trainable):
         with tf.variable_scope(name):
@@ -105,20 +92,11 @@ class PPO(object):
         params = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=name)
         return norm_dist, params
 
-    def _build_critic_net(self, scope):
-        h1_units = 100
-        with tf.variable_scope(scope):
-            l1 = tf.layers.dense(self.tfs, h1_units, tf.nn.relu)
-            critic_net = tf.layers.dense(l1, 1)
-        return critic_net
-
     def choose_action(self, s):
         s = s[np.newaxis, :]
         a = self.sess.run(self.sample_op, {self.tfs: s})[0]
         return np.clip(a, -2, 2)
 
     def get_v(self, s):
-        if s.ndim < 2:
-            s = s[np.newaxis, :]
-        return self.sess.run(self.critic_net, {self.tfs: s})[0, 0]
-
+        if s.ndim < 2: s = s[np.newaxis, :]
+        return self.sess.run(self.v, {self.tfs: s})[0, 0]
--- Reinforcement Learning/PPO/run.py
+++ Reinforcement Learning/PPO/run.py
@@ -1,5 +1,6 @@
 import gym
-from RLcore import PPO
+from RLcore_PPO import PPO
+from filter import ZFilter
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -15,13 +16,16 @@ METHOD = [
     dict(name='clip', epsilon=0.2),                 # Clipped surrogate objective, find this is better
 ][1]        # choose the method for optimization
 
-
 env = gym.make('Pendulum-v0').unwrapped
 ppo = PPO()
 all_ep_r = []
 
+running_state = ZFilter((env.observation_space.shape[0],), clip=10.0)
+return_filter = ZFilter((1,), clip=20.0)
+
 for ep in range(EP_MAX):
     s = env.reset()
+    s = running_state(s)
     buffer_s, buffer_a, buffer_r = [], [], []
     ep_r = 0
     for t in range(EP_LEN):    # in one episode
@@ -32,6 +36,7 @@ for ep in range(EP_MAX):
         buffer_a.append(a)
         buffer_r.append(r)
         s = s_
+        s = running_state(s)
         ep_r += r
 
         # update ppo
@@ -40,11 +45,12 @@ for ep in range(EP_MAX):
             discounted_r = []
             for r in buffer_r[::-1]:
                 v_s_ = r + GAMMA * v_s_
+                v_s_ = return_filter([v_s_])[0]
                 discounted_r.append(v_s_)
             discounted_r.reverse()
 
-            bs, ba, bv = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
-            ppo.update(bs, ba, np.vstack(buffer_r), bv)
+            bs, ba, br = np.vstack(buffer_s), np.vstack(buffer_a), np.array(discounted_r)[:, np.newaxis]
+            ppo.update(bs, ba, br)
             buffer_s, buffer_a, buffer_r = [], [], []
     if ep == 0:
         all_ep_r.append(ep_r)
@@ -57,6 +63,7 @@ for ep in range(EP_MAX):
     )
 
 plt.plot(np.arange(len(all_ep_r)), all_ep_r)
-plt.xlabel('Episode');
-plt.ylabel('Moving averaged episode reward');
+plt.xlabel('Episode')
+plt.ylabel('Moving averaged episode reward')
 plt.show()
+
