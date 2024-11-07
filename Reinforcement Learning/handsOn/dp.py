615fd0a88f89b05c8209a52ab944cbebb9c05622
swliu <liushuwei1991@gmail.com>
2024-11-07 23:53:58 +0800

    [feat] add value iteration

--- Reinforcement Learning/handsOn/dp.py
+++ Reinforcement Learning/handsOn/dp.py
@@ -57,3 +57,47 @@ class PolicyIteration:
             new_pi = self.policy_improvement()
             if old_pi == new_pi:
                 break
+
+
+class ValueIteration:
+    def __init__(self, env, theta, gamma):
+        self.env = env
+        self.v = [0] * self.env.ncol * self.env.nrow
+        self.theta = theta
+        self.gamma = gamma
+        self.pi = [None for i in range(self.env.ncol * self.env.nrow)]
+
+    def value_iteration(self):
+        cnt = 0
+        while 1:
+            max_diff = 0
+            new_v = [0] * self.env.ncol * self.env.nrow
+            for s in range(self.env.ncol * self.env.nrow):
+                qsa_list = []  # 开始计算状态s下的所有Q(s,a)价值
+                for a in range(4):
+                    qsa = 0
+                    for res in self.env.P[s][a]:
+                        p, next_state, r, done = res
+                        qsa += p * (r + self.gamma * self.v[next_state] * (1 - done))
+                    qsa_list.append(qsa)  # 这一行和下一行代码是价值迭代和策略迭代的主要区别
+                new_v[s] = max(qsa_list)
+                max_diff = max(max_diff, abs(new_v[s] - self.v[s]))
+            self.v = new_v
+            if max_diff < self.theta: break  # 满足收敛条件,退出评估迭代
+            cnt += 1
+        print("价值迭代一共进行%d轮" % cnt)
+        self.get_policy()
+
+    def get_policy(self):  # 根据价值函数导出一个贪婪策略
+        for s in range(self.env.nrow * self.env.ncol):
+            qsa_list = []
+            for a in range(4):
+                qsa = 0
+                for res in self.env.P[s][a]:
+                    p, next_state, r, done = res
+                    qsa += p * (r + self.gamma * self.v[next_state] * (1 - done))
+                qsa_list.append(qsa)
+            maxq = max(qsa_list)
+            cntq = qsa_list.count(maxq)  # 计算有几个动作得到了最大的Q值
+            # 让这些动作均分概率
+            self.pi[s] = [1 / cntq if q == maxq else 0 for q in qsa_list]
