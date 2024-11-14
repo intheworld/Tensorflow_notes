49901cdc613cf045c7ec5cc702b68492c4373b62
swliu <liushuwei1991@gmail.com>
2024-11-14 23:37:22 +0800

    [feat] add part of SAC

--- Reinforcement Learning/handsOn/ddpg.py
+++ Reinforcement Learning/handsOn/ddpg.py
@@ -30,7 +30,7 @@ class QValueNet(torch.nn.Module):
         cat = torch.cat([x, a], dim=1)
         x = F.relu(self.fc1(cat))
         x = F.relu(self.fc2(x))
-        return self.fc2(x)
+        return self.fc_out(x)
 
 
 class DDPG:
---
+++ Reinforcement Learning/handsOn/sac.py
@@ -0,0 +1,81 @@
+import gym
+import torch
+import torch.nn.functional as F
+import numpy as np
+import random
+import rl_utils
+import matplotlib.pyplot as plt
+
+class PolicyNet(torch.nn.Module):
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(PolicyNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return F.softmax(self.fc2(x), dim=1)
+
+
+class QValueNet(torch.nn.Module):
+    ''' 只有一层隐藏层的Q网络 '''
+    def __init__(self, state_dim, hidden_dim, action_dim):
+        super(QValueNet, self).__init__()
+        self.fc1 = torch.nn.Linear(state_dim, hidden_dim)
+        self.fc2 = torch.nn.Linear(hidden_dim, action_dim)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        return self.fc2(x)
+
+
+class SAC:
+    def __init__(self, state_dim, hidden_dim, action_dim, actor_lr, critic_lr,
+                 alpha_lr, target_entropy, tau, gamma, device):
+        # 策略网络
+        self.actor = PolicyNet(state_dim, hidden_dim, action_dim).to(device)
+        # 第一个Q网络
+        self.critic_1 = QValueNet(state_dim, hidden_dim, action_dim).to(device)
+        # 第二个Q网络
+        self.critic_2 = QValueNet(state_dim, hidden_dim, action_dim).to(device)
+        self.target_critic_1 = QValueNet(state_dim, hidden_dim,
+                                         action_dim).to(device)  # 第一个目标Q网络
+        self.target_critic_2 = QValueNet(state_dim, hidden_dim,
+                                         action_dim).to(device)  # 第二个目标Q网络
+        # 令目标Q网络的初始参数和Q网络一样
+        self.target_critic_1.load_state_dict(self.critic_1.state_dict())
+        self.target_critic_2.load_state_dict(self.critic_2.state_dict())
+        self.actor_optimizer = torch.optim.Adam(self.actor.parameters(),
+                                                lr=actor_lr)
+        self.critic_1_optimizer = torch.optim.Adam(self.critic_1.parameters(),
+                                                   lr=critic_lr)
+        self.critic_2_optimizer = torch.optim.Adam(self.critic_2.parameters(),
+                                                   lr=critic_lr)
+        # 使用alpha的log值,可以使训练结果比较稳定
+        self.log_alpha = torch.tensor(np.log(0.01), dtype=torch.float)
+        self.log_alpha.requires_grad = True  # 可以对alpha求梯度
+        self.log_alpha_optimizer = torch.optim.Adam([self.log_alpha],
+                                                    lr=alpha_lr)
+        self.target_entropy = target_entropy  # 目标熵的大小
+        self.gamma = gamma
+        self.tau = tau
+        self.device = device
+
+    def take_action(self, state):
+        state = torch.tensor([state], dtype=torch.float).to(self.device)
+        probs = self.actor(state)
+        action_dist = torch.distributions.Categorical(probs)
+        action = action_dist.sample()
+        return action.item()
+
+    def calc_target(self, rewards, next_states, dones):
+        next_probs = self.actor(next_states)
+        next_log_probs = torch.log(next_probs + 1e-8)
+        entropy = -torch.sum(next_probs * next_log_probs, dim=1, keepdim=True)
+        q1_value = self.target_critic_1(next_states)
+        q2_value = self.target_critic_2(next_states)
+        min_qvalue = torch.sum(next_probs * torch.min(q1_value, q2_value), dim=1, keepdim=True)
+
+        next_value = min_qvalue + self.log_alpha.exp() * entropy
+        td_target = rewards + self.gamma * next_value * (1 - dones)
+        return td_target
