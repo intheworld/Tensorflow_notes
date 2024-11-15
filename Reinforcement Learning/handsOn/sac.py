943cef1c34bd03950947ddd9441983e4bb41df8d
swliu <liushuwei1991@gmail.com>
2024-11-15 21:41:21 +0800

    [feat] add discrete SAC

--- Reinforcement Learning/handsOn/sac.py
+++ Reinforcement Learning/handsOn/sac.py
@@ -3,6 +3,9 @@ import torch
 import torch.nn.functional as F
 import numpy as np
 import random
+
+from sympy.physics.quantum.density import entropy
+
 import rl_utils
 import matplotlib.pyplot as plt
 
@@ -74,8 +77,115 @@ class SAC:
         entropy = -torch.sum(next_probs * next_log_probs, dim=1, keepdim=True)
         q1_value = self.target_critic_1(next_states)
         q2_value = self.target_critic_2(next_states)
+        # Q: 这里为什么要乘以 next_probs？QValueNet本身也是输出action维度？
+        # A: 因为这里的 QValueNet 其实是动作价值函数——Q(s,a)
         min_qvalue = torch.sum(next_probs * torch.min(q1_value, q2_value), dim=1, keepdim=True)
 
         next_value = min_qvalue + self.log_alpha.exp() * entropy
         td_target = rewards + self.gamma * next_value * (1 - dones)
         return td_target
+
+    def soft_update(self, net: QValueNet, target_net: QValueNet):
+        for param_target, param in zip(target_net.parameters(), net.parameters()):
+            param_target.data.copy_(param_target.data * (1.0 - self.tau) + param.data * self.tau)
+
+
+    def update(self, transition_dict):
+        states = torch.tensor(transition_dict['states'],
+                              dtype=torch.float).to(self.device)
+        actions = torch.tensor(transition_dict['actions']).view(-1, 1).to(self.device)
+        rewards = torch.tensor(transition_dict['rewards'],
+                               dtype=torch.float).view(-1, 1).to(self.device)
+        next_states = torch.tensor(transition_dict['next_states'],
+                                   dtype=torch.float).to(self.device)
+        dones = torch.tensor(transition_dict['dones'],
+                             dtype=torch.float).view(-1, 1).to(self.device)
+        # 和之前章节一样,对倒立摆环境的奖励进行重塑以便训练
+        rewards = (rewards + 8.0) / 8.0
+
+        # update Q-net
+        td_target = self.calc_target(rewards, next_states, dones)
+        critic_1_q_values = self.critic_1(states).gather(1, actions)
+        critic_1_loss = torch.mean(
+            F.mse_loss(critic_1_q_values, td_target.detach()))
+        critic_2_q_values = self.critic_2(states).gather(1, actions)
+        critic_2_loss = torch.mean(
+            F.mse_loss(critic_2_q_values, td_target.detach()))
+        self.critic_1_optimizer.zero_grad()
+        critic_1_loss.backward()
+        self.critic_1_optimizer.step()
+        self.critic_2_optimizer.zero_grad()
+        critic_2_loss.backward()
+        self.critic_2_optimizer.step()
+
+        # update policy net
+        probs = self.actor(states)
+        log_probs = torch.log(probs + 1e-8)
+
+        entropy = -torch.sum(probs * log_probs, dim=1, keepdim=True)
+        q1_value = self.critic_1(states)
+        q2_value = self.critic_2(states)
+
+        min_qvalue = torch.sum(probs * torch.min(q1_value, q2_value), dim=1, keepdim=True)
+
+        actor_loss = torch.mean(-self.log_alpha.exp() * entropy - min_qvalue)
+        self.actor_optimizer.zero_grad()
+        actor_loss.backward()
+        self.actor_optimizer.step()
+
+        # update alpha
+        alpha_loss = torch.mean(
+            (entropy - self.target_entropy).detach() * self.log_alpha.exp())
+        self.log_alpha_optimizer.zero_grad()
+        alpha_loss.backward()
+        self.log_alpha_optimizer.step()
+
+        self.soft_update(self.critic_1, self.target_critic_1)
+        self.soft_update(self.critic_2, self.target_critic_2)
+
+
+actor_lr = 1e-3
+critic_lr = 1e-2
+alpha_lr = 1e-2
+num_episodes = 200
+hidden_dim = 128
+gamma = 0.98
+tau = 0.005  # 软更新参数
+buffer_size = 10000
+minimal_size = 500
+batch_size = 64
+target_entropy = -1
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+env_name = 'CartPole-v0'
+env = gym.make(env_name)
+random.seed(0)
+np.random.seed(0)
+env.seed(0)
+torch.manual_seed(0)
+replay_buffer = rl_utils.ReplayBuffer(buffer_size)
+state_dim = env.observation_space.shape[0]
+action_dim = env.action_space.n
+agent = SAC(state_dim, hidden_dim, action_dim, actor_lr, critic_lr, alpha_lr,
+            target_entropy, tau, gamma, device)
+
+return_list = rl_utils.train_off_policy_agent(env, agent, num_episodes,
+                                              replay_buffer, minimal_size,
+                                              batch_size)
+
+episodes_list = list(range(len(return_list)))
+plt.plot(episodes_list, return_list)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('SAC on {}'.format(env_name))
+plt.show()
+
+mv_return = rl_utils.moving_average(return_list, 9)
+plt.plot(episodes_list, mv_return)
+plt.xlabel('Episodes')
+plt.ylabel('Returns')
+plt.title('SAC on {}'.format(env_name))
+plt.show()
+
+
+
