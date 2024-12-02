134928b99fe05065193697ca0ff30fd74bbe802a
swliu <liushuwei1991@gmail.com>
2024-12-02 23:34:27 +0800

    [feat] finish Transformer class

--- transformer/model.py
+++ transformer/model.py
@@ -146,4 +146,70 @@ class Encoder(nn.Module):
         return self.norm(x)
 
 
+class DecodeBlock(nn.Module):
 
+    def __init__(self, features: int, self_attention_block: MultiHeadAttentionBlock, 
+                 cross_attention_block: MultiHeadAttentionBlock, feed_forward_block: FeedForwardBlock, 
+                 dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidualConnection(features, dropout) for _ in range(3)])
+
+        def forward(self, x, encoder_output, src_mask, tgt_mask):
+            x = self.residual_connections[0](x, lambda x: self.self_attension_block(x, x, x, tgt_mask))
+            x = self.residual_connections[1](x, lambda x: self.cross_attention_block(x, encoder_output, encoder_output, src_mask))
+            x = self.residual_connections[2](x, lambda x: self.feed_forward_block)
+            return x
+        
+
+class Decoder(nn.Module):
+    def __init__(self, features: int, layers: nn.ModuleList):
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization(features)
+
+    def forward(self, x, encoder_output, src_mask, tgt_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, tgt_mask)
+        return self.norm(x)
+    
+
+class ProjectionLayer(nn.Module):
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
+        self.proj = nn.Linear(d_model, vocab_size)
+
+    def forward(self, x) -> None:
+        # (batch, seq_len, d_model) --> (batch, seq_len, vocab_size)
+        return self.proj(x)
+
+
+class Transformer(nn.Module):
+    def __init__(self, encoder: Encoder, decoder: Decoder, src_embed: InputEmbedings, tgt_embed: InputEmbedings, 
+                 src_pos: PositionalEncoding, tgt_pos: PositionalEncoding, projection_layer: ProjectionLayer) -> None:
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.src_embed = src_embed
+        self.tgt_embed = tgt_embed
+        self.src_pos = src_pos
+        self.tgt_pos = tgt_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, src, src_mask):
+        # (batch, seq_len, d_model)
+        src = self.src_embed(src)
+        src = self.src_pos(src)
+        return self.encoder(src, src_mask)
+    
+    def decode(self, encoder_output: torch.Tensor, src_mask: torch.Tensor, tgt: torch.Tensor, tgt_mask: torch.Tensor):
+        # (batch, seq_len, d_model)
+        tgt = self.tgt_embed(tgt)
+        tgt = self.tgt_pos(tgt)
+        return self.decoder(tgt, encoder_output, src_mask, tgt_mask)
+    
+    def projection(self, x):
+        # (batch, seq_len, vocab_size)
+        return self.projection_layer(x)
