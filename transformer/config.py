f6e8b58eb4fafb01399a77cbc4edea441c6cc812
swliu <liushuwei1991@gmail.com>
2024-12-16 23:57:06 +0800

    [opt] resize batch_size to 1

--- transformer/config.py
+++ transformer/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 def get_config():
     return {
-        "batch_size": 8,
+        "batch_size": 1,
         "num_epochs": 20,
         "lr": 10**-4,
         "seq_len": 350,
