3e62e6808b0a1e74db1ae047e483a3e6e5799d2d
swliu <liushuwei1991@gmail.com>
2024-12-24 21:41:44 +0800

    [feat] add vlm embedding

--- vision_language_model/modeling_siglip.py
+++ vision_language_model/modeling_siglip.py
@@ -29,3 +29,42 @@ class SiglipVisonConfig:
         self.layer_norm_eps = layer_norm_eps,
         self.attention_dropout = attention_dropout,
         self.num_image_tokens = num_image_tokens
+
+
+class SiglipVisionEmbedding(nn.Module):
+    def __init__(self, config: SiglipVisonConfig):
+        super().__init__()
+        self.config = config
+        self.embed_dim = config.hidden_size
+        self.image_size = config.image_size
+        self.patch_size = config.patch_size
+
+        self.patch_embedding = nn.Conv2d(
+            in_channels=config.num_channels,
+            out_channels=self.embed_dim,
+            kernel_size=self.patch_size,
+            stride=self.patch_size,
+            padding="valid"
+        )
+
+        self.num_patches = (self.image_size // self.patch_size) ** 2
+        self.num_positions = self.num_patches
+        self.position_embedding = nn.Embedding(self.num_positions, self.embed_dim)
+        self.register_buffer(
+            "position_ids",
+            torch.arange(self.num_positions).expand((1, -1)),
+            persistent=False
+        )
+
+    def forward(self, pixel_values: torch.FloatTensor) -> torch.Tensor:
+        # [Batch_Size, Channels, Height, Width]
+        _, _, height, width = pixel_values.shape
+        # [Batch_Size, Channels, Height, Width] => [Batch_Size, Embed_Dim, Num_Patches_H, Num_Patches_W]
+        patch_embeds = self.patch_embedding(pixel_values)
+        # [Batch_Size, Embed_Dim, Num_Patches_H, Num_Patches_W] -> [Batch_Size, Embed_Dim, Num_Patches]
+        embeddings = patch_embeds.flatten(2)
+        # [Batch_Size, Embed_Dim, Num_Patches] -> [Batch_Size, Num_Patches, Embed_Dim]
+        embeddings = embeddings.transpose(1, 2)
+        embeddings = embeddings + self.position_embedding(self.position_ids)
+
+        return embeddings
