71896034a460ede216dfd7feac4d4bf54791221d
swliu <liushuwei1991@gmail.com>
2025-01-18 21:56:48 +0800

    [feat] finish gemma model

--- vision_language_model/modeling_gemma.py
+++ vision_language_model/modeling_gemma.py
@@ -403,7 +403,7 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         return self.language_model.tie_weights()
     
     def _merge_input_ids_with_image_features(
-        self, image_features: torch.Tensor, inputs_embeds: torch.Tensor, input_ids: torch.Tensor, attension_mask: torch.Tensor, kv_cache: Optional[KVCache] = None
+        self, image_features: torch.Tensor, inputs_embeds: torch.Tensor, input_ids: torch.Tensor, attention_mask: torch.Tensor, kv_cache: Optional[KVCache] = None
     ):
         _, _, embed_dim = image_features.shape
         batch_size, sequence_length = input_ids.shape
@@ -422,3 +422,59 @@ class PaliGemmaForConditionalGeneration(nn.Module):
         text_mask_expanded = text_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
         pad_mask_expanded = pad_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
         image_mask_expanded = image_mask.unsqueeze(-1).expand(-1, -1, embed_dim)
+
+        final_embedding = torch.where(text_mask_expanded, inputs_embeds, final_embedding)
+        final_embddding = final_embedding.masked_scatter(image_mask_expanded, scaled_image_features)
+        final_embddding = torch.where(pad_mask_expanded, torch.zeros_like(final_embddding), final_embddding)
+        
+        dtype, device = inputs_embeds.dtype, inputs_embeds.device
+        min_dtype = torch.finfo(dtype).min
+        q_len = inputs_embeds.shape[1]
+
+        if kv_cache in None or kv_cache.num_items() == 0:
+            causal_mask = torch.full(
+                (batch_size, q_len, q_len), fill_value=0, dtype=dtype, device=device
+            )
+        else:
+            assert q_len == 1
+            kv_len = kv_cache.num_items() + q_len
+            causal_mask = torch.full(
+                (batch_size, q_len, kv_len), fill_value=0, dtype=dtype, device=device
+            )
+        
+        causal_mask = causal_mask.unsqueeze(1)
+
+        if kv_cache is not None and kv_cache.num_items() > 0:
+            position_ids = attention_mask.cumsum(-1)[:, -1]
+            if position_ids.dim() == 1:
+                position_ids = position_ids.unsqueeze(0)
+        else:
+            position_ids = (attention_mask.cumsum(-1)).masked_fill_((attention_mask == 0), 1).to(device)
+
+        return final_embddding, causal_mask, position_ids
+    
+    def forward(
+        self,
+        input_ids: torch.LongTensor = None,
+        pixel_values: torch.FloatTensor = None,
+        attention_mask: Optional[torch.Tensor] = None,
+        kv_cache: Optional[KVCache] = None
+    ) -> Tuple:
+        assert torch.all(attention_mask == 1), "The input cannot be padded"
+
+        inputs_embeds = self.language_model.get_input_embeddings()(input_ids)
+
+        selected_image_feature = self.vision_tower(pixel_values.to(inputs_embeds.dtype))
+
+        image_features = self.multi_modal_projector(selected_image_feature)
+
+        inputs_embeds, attention_mask, position_ids = self._merge_input_ids_with_image_features(image_features, inputs_embeds, input_ids, attention_mask, kv_cache)
+
+        outputs = self.language_model(
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            inputs_embeds=inputs_embeds,
+            kv_cache=kv_cache
+        )
+
+        return outputs
