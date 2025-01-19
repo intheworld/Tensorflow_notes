f3117ea1b9c4cab364a3d824b42df514bf6c6cbc
swliu <liushuwei1991@gmail.com>
2025-01-19 21:06:32 +0800

    [feat] create util and env

--- vision_language_model/utils.py
+++ vision_language_model/utils.py
@@ -1,5 +1,34 @@
+from modeling_gemma import PaliGemmaForConditionalGeneration, PaliGemmaConfig
+from transformers import AutoTokenizer
 import json
 import glob
+from safetensors import safe_open
 from typing import Tuple
 import os
+
+
+def load_hf_model(model_path: str, device: str) -> Tuple[PaliGemmaForConditionalGeneration, AutoTokenizer]:
+    tokenizer = AutoTokenizer.from_pretrained(model_path, padding_side="right")
+    assert tokenizer.padding_side == "right"
+
+    safetensors_files = glob.glob(os.path.join(model_path, "*.safetensors"))
+
+    tensors = {}
+    for safetensors_file in safetensors_files:
+        with safe_open(safetensors_file, framework="pt", device="cpu") as f:
+            for key in f.keys():
+                tensors[key] = f.get_tensor(key)
+        
+    with open(os.path.join(model_path, "config.json"), "r") as f:
+        model_config_file = json.load(f)
+        config = PaliGemmaConfig(**model_config_file)
+    
+    model = PaliGemmaForConditionalGeneration(config).to(device)
+
+    model.load_state_dict(tensors, strict=False)
+
+    model.tie_weights()
+
+    return (model, tokenizer)
+
 
