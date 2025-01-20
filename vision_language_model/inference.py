81b50339780a022784bdad778cded2ca033b8913
swliu <liushuwei1991@gmail.com>
2025-01-20 23:37:12 +0800

    [feat] add inference file

---
+++ vision_language_model/inference.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import torch
+import fire
+
+from processing_paligemma import PaliGemmaProcessor
+from modeling_gemma import KVCache, PaliGemmaForConditionalGeneration
+from utils import load_hf_model
+
+
+def move_inputs_to_device(model_inputs: dict, device: str):
+    model_inputs = {k: v.to(device) for k, v in model_inputs.items()}
+    return model_inputs
+
+
+def get_model_inputs(
+    processor: PaliGemmaProcessor, prompt: str, image_file_path: str, device: str
+):
+    image = Image.open(image_file_path)
+    images = [image]
+    prompts = [prompt]
+    model_inputs = processor(text=prompts, images=images)
+    model_inputs = move_inputs_to_device(model_inputs, device)
+    return model_inputs
